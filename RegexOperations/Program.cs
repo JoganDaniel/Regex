2dfd9482b03f2927af93a323fe66eb2319c3b29a
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-02 16:16:53 +0530

    [jogan] Validation for first name done

--- RegexOperations/Program.cs
+++ RegexOperations/Program.cs
@@ -1,30 +1,19 @@
-﻿using System;
+﻿using RegexOperations.UserRegistrationSystem;
+using System;
 using System.Windows.Markup;
 
 namespace RegexOperations
 {
     public class Program
     {
-        //static string email1 = "abc";
-        //static string email2 = "@bridgelabz";
-        //static string email3 = ".com";
-        //static string email4 = "abc.hh@bridgelabz.com";
-        //static string email5 = "abc.de@bridgelabz.co.in";
-        static string[] emails = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+        static string firstName = "Aweff";
         
         private static void Main(string[] args)
         {
-            ValidateEmail validateEmail = new ValidateEmail();
-            //validateEmail.uc1Validate(email1);
-            //validateEmail.uc2Validate(email2);
-            //validateEmail.uc3Validate(email3);
-            //validateEmail.uc4Validate(email4);
-            //validateEmail.uc5Validate(email5);
-            foreach (string email in emails)
-            {
-                Console.WriteLine(email);
-                validateEmail.uc5Validate(email);
-            }
+            UserRegistration validate = new UserRegistration();
+            Console.WriteLine(firstName);
+            validate.ValidateName(firstName);
+          
         }
     }
 }
---
+++ RegexOperations/UserRegistrationSystem/UserRegistration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexOperations.UserRegistrationSystem
+{
+    public class UserRegistration
+    {
+        string nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+        string emailRegex = "^[a-z0-9]{1,}[._+-]{0,1}[a-z0-9]{0,}[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]{0,1}[a-z]{0,}$";
+        string mobileRegex = "^[0-9]{2}[ ][0-9]{10}";
+        string passwordRegex = "^[a-z0-9]{8}";
+        public void ValidateName(string name)
+        {
+            if(Regex.IsMatch(name, nameRegex))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+        public void ValidateEmail(string mail)
+        {
+            if (Regex.IsMatch(mail, emailRegex))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+        public void ValidateMobile(string number)
+        {
+            if (Regex.IsMatch(number, mobileRegex))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+    }
+}
