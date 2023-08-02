f3ac93f11ae4312c4a4787466725679ae863f31e
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-02 13:31:31 +0530

    [jogan] Validate email UC4 uploaded

--- RegexOperations/Program.cs
+++ RegexOperations/Program.cs
@@ -6,6 +6,9 @@ namespace RegexOperations
         static string email1 = "abc";
         static string email2 = "@bridgelabz";
         static string email3 = ".com";
+        static string email4 = "abc.hh@bridgelabz.com";
+        static string email5 = "abc.hh@bridgelabz.com";
+
 
         private static void Main(string[] args)
         {
@@ -13,6 +16,8 @@ namespace RegexOperations
             validateEmail.uc1Validate(email1);
             validateEmail.uc2Validate(email2);
             validateEmail.uc3Validate(email3);
+            validateEmail.uc4Validate(email4);
+
         }
     }
 }
--- RegexOperations/ValidateEmail.cs
+++ RegexOperations/ValidateEmail.cs
@@ -13,8 +13,8 @@ namespace RegexOperations
         string uc1Regex = "^[a-z]{0,}$";
         string uc2Regex = "^[@]{1}[a-z]{1,}$";
         string uc3Regex = "^[.]{1}[a-z]{2,3}$";
-        string uc4Regex = "^[a-z]+[._+-]{0,1}[a-z]&";
-        string actualRegex = "^[a-z]+([._+-]{0,1}[a-z]+[@]";
+        string uc4Regex = "^[a-z]+[._+-]{0,1}[a-z]{0,}[@]{1}[a-z]{1,}[.]{1}[a-z]{2,}$";
+        string actualRegex = "^[a-z]+[._+-]{0,1}[a-z]{1,}[@]{1}[a-z]{1,}[.]{1}[a-z]{2,3}+[.]{0,1}+[a-z]{0,2}$";
         public void uc1Validate(string input)
         {
             bool result = Regex.IsMatch(input, uc1Regex);
@@ -47,5 +47,6 @@ namespace RegexOperations
             else
                 Console.WriteLine("! inValid");
         }
+
     }
 }
